869dc85c648fe1c04d926c580cadf5828119d374
stan_liao <donkeystan@gmail.com>
2022-10-24 11:28:20 +0800

    2022_10_24

--- Hard/0023_Merge_k_Sorted_Lists/0023_Merge_k_Sorted_Lists.c
+++ Hard/0023_Merge_k_Sorted_Lists/0023_Merge_k_Sorted_Lists.c
@@ -20,6 +20,7 @@ struct ListNode *insert(struct ListNode *head, int val);
 struct ListNode *append_node(struct ListNode *head, struct ListNode **last, int val);
 
 struct ListNode *merge(struct ListNode *list1, struct ListNode *list2);
+struct ListNode *merge_lists(struct ListNode *list1, struct ListNode *list2);
 struct ListNode *get_node(int val);
 
 struct ListNode* mergeKLists(struct ListNode** lists, int listsSize)
@@ -48,6 +49,30 @@ struct ListNode* mergeKLists(struct ListNode** lists, int listsSize)
     }
     
     return head;
+}
+
+struct ListNode *merge_lists(struct ListNode *list1, struct ListNode *list2)
+{
+    if (NULL == list1)
+    {
+        return list2;
+    }
+
+    if (NULL == list2)
+    {
+        return list1;
+    }
+
+    if (list1->val <= list2->val)
+    {
+        list1->next = merge_lists(list1->next, list2);
+        return list1;
+    }
+    else
+    {
+        list2->next = merge_lists(list2->next, list1);
+        return list2;
+    }
 }
 
 struct ListNode *merge(struct ListNode *list1, struct ListNode *list2)
